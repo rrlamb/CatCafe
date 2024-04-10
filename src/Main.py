78e5507d121fb5bd160a3796b7b9238994fde519
rrlamb <89356204+rrlamb@users.noreply.github.com>
2024-04-10 15:00:12 -0400

    Connected to local database with local user. Can insert into cat table

--- src/Main.py
+++ src/Main.py
@@ -1,16 +1,27 @@
-# this is place holder code not perfect yet
-
 import mysql.connector
 
 def main():
     try:
         cafe = mysql.connector.connect(
-            host="localhost",
-            user="yourusername",
-            password="yourpassword",
-            database="yourdatabase"
+            host="127.0.0.1",  # Hostname (equivalent to localhost)
+            port=3306,  # Port number
+            user="catcafe",  # MySQL user
+            password="CoffeeCatSandwich",  # Password (you can provide your password here if any) # Database/schema (no specific database selected)
+            database="CatCafeInfo",
         )
         print("Connected")
+
+        cursor = cafe.cursor()
+
+        cat_insert = "INSERT INTO Cat (cat_name, breed, sex, age, weight, adoption_phone_number) VALUES (%s, %s, %s, %s, %s, %s)"
+        cat_data = ('Daisy', 'American Shorthair', 'F', 1, 5, '1111111111')
+
+        cursor.execute(cat_insert, cat_data)
+
+        cafe.commit()
+
+        print("Added cat")
+
     except mysql.connector.Error as e:
         print("Cannot connect to database:", e)
 
